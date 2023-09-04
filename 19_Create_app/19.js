0c81da059675824fa18efa835201279e536e6bf0
Lyuhon <lyuhon.uz@gmail.com>
2023-09-05 00:52:07 +0500

    20 Лекция - условные операторы

---
+++ 19_Create_app/19.js
@@ -0,0 +1,27 @@
+const numberOfFilms = +prompt('Сколько фильмов?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {
+
+    },
+    actors: {
+
+    },
+    geners: [],
+    privat: false
+};
+
+const question_1 = prompt('Послежний фильм?'),
+    question_2 = prompt('Оценка?');
+
+const question_3 = prompt('Послежний фильм?'),
+    question_4 = prompt('Оценка?');
+
+personalMovieDB.movies[question_1] = question_2;
+// можно было записать personalMovieDB.movies.question_1 = ...
+// но так бы не работало с пробелами
+personalMovieDB.movies[question_3] = question_4;
+
+console.log(personalMovieDB);
+document.write(personalMovieDB);
--- 19_Create_app/20.js
+++ 19_Create_app/20.js
@@ -1,31 +1,3 @@
-const numberOfFilms = +prompt('Сколько фильмов?', '');
-
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {
-
-    },
-    actors: {
-
-    },
-    geners: [],
-    privat: false
-};
-
-const question_1 = prompt('Послежний фильм?'),
-    question_2 = prompt('Оценка?');
-
-const question_3 = prompt('Послежний фильм?'),
-    question_4 = prompt('Оценка?');
-
-personalMovieDB.movies[question_1] = question_2;
-// можно было записать personalMovieDB.movies.question_1 = ...
-// но так бы не работало с пробелами
-personalMovieDB.movies[question_3] = question_4;
-
-console.log(personalMovieDB);
-document.write(personalMovieDB);
-
 // 20 Лекция
 
 if (4 === '4') { // можно поставить условие 1+ то будет правда
