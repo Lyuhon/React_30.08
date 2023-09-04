d5a898dd01ff91189132e9ffba60930f1f9baf2e
Lyuhon <lyuhon.uz@gmail.com>
2023-09-04 23:14:23 +0500

    Сообщение о коммите

---
+++ index.js
@@ -0,0 +1,39 @@
+// let number = 15;
+// const leftBorderWidth = 1;
+
+// number = 10;
+// console.log(number);
+
+// const obj = {
+//     mother: 'nigga',
+//     father: 'super-nigga'
+// };
+
+// console.log(obj.father);
+
+// obj.grand = 'her-usatiy';
+// console.log(obj['grand']);
+// console.log(obj.mother);
+
+// const result = confirm('Норм сайт?');
+// console.log(confirm);
+
+// const answer = prompt('Тебе 20?', '20');
+// console.log(typeof (answer));
+
+// const answers = [];
+
+// answers[0] = prompt('Как зовут?', '');
+// answers[1] = prompt('Как фамилия?', '');
+// answers[2] = prompt('Сколько лет?', '');
+
+// document.write(answers); // устаревшая команда, заменяет весь текст
+// null тип является объектом
+
+let incr = 10,
+    decr = 10;
+
+incr++;
+decr--;
+
+console.log(incr++, --decr);
